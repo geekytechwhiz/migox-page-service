32c2000544b465b147262550b76db8649333c320
Prasanth <prashanthmsktm@gmail.com>
2024-04-26 13:05:03 +0530

    Initial commit

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,11 +1,11 @@
-import AWS from 'aws-sdk'; 
+import AWS from 'aws-sdk';
 const dynamoDB = new AWS.DynamoDB();
 const documentClient = new AWS.DynamoDB.DocumentClient();
 export { dynamoDB, documentClient };
 
 export const TWILIO_CONFIG = {
-  fromNumber: process.env.twilio_fromNumber || '+15017122661',
-  accountSid:
-    process.env.twilio_accountSid || 'AC4456edea3518c403c56f653e6dcf1519',
-  authToken: process.env.twilio_authToken || 'bdaec2b5cc14f5814c2d1f3f6c6661bc',
+  // fromNumber: process.env.twilio_fromNumber || '+15017122661',
+  // accountSid:
+  //   process.env.twilio_accountSid || 'AC4456edea3518c403c56f653e6dcf1519',
+  // authToken: process.env.twilio_authToken || 'bdaec2b5cc14f5814c2d1f3f6c6661bc',
 };
